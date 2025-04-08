4ccbdddfcc31e5be448206c6df9c9e7e572a10cd
Caleb Wharton <calebwharton123@gmail.com>
2025-04-08 12:18:20 +1200

    added sections of landing page, and play font

--- client/src/app/landingpage/header.tsx
+++ client/src/app/landingpage/header.tsx
@@ -1,4 +1,3 @@
-import play from '../layout';
 import frank from '../../../public/frank.svg';
 import squaremenu from '../../../public/square-menu.svg';
 
@@ -6,8 +5,8 @@ export default function Header() {
   return (
     <div className="flex items-center justify-between h-32 border-b border-gray-300 bg-white shadow-md ">
       <img src={frank.src} alt="Frank" className="w-16 h-16 ml-10" />
-      <h1 className="text-blue-800 text-4xl font-bold mr-12">Labchat</h1>
-      <img src={squaremenu.src} alt="burger menu" className="w-16 h-16 mr-4" />
+      <h1 className="text-blue-800 text-4xl font-bold mr-12 play-font">Labchat</h1>
+      <img src={squaremenu.src} alt="burger menu" className="w-12 h-12 mr-4" />
     </div>
   );
 }
