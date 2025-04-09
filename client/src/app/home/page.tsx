cfd87e7ada49b74b37d28fe4a59f3ebaa0e77f2f
Caleb Wharton <calebwharton123@gmail.com>
2025-04-09 14:53:50 +1200

    added stars

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -9,6 +9,8 @@ import star2 from '../../../public/star2.svg';
 import car from '../../../public/car.svg';
 import computer from '../../../public/computer.svg';
 import Footer from "./footer";
+import aboutStar from '../../../public/aboutStar.svg';
+import chooseStar from '../../../public/chooseStar.svg';
 
 
 export default function Home() {
@@ -20,16 +22,25 @@ export default function Home() {
         <h3 className="text-neutral-500 font-bold text-l mb-1">Introducing Labchat: An LMS for universities</h3>
         <h2 className="text-black font-bold text-4xl">Streamlining labs to <span className="text-[#244394] font-bold text-4xl">empower research</span></h2>
       </div>
-      <div className="flex justify-start mt-5 play-font">
-          <Button className="bg-[#284AA3] text-white font-medium ml-0 py-3  hover:bg-blue-300 transition duration-300"><span className="pr-18">Login</span> <ArrowRight className="w-4 h-4" /></Button>
-          <Button className="bg-white text-neutral-800 font-medium py-2 ml-4  border border-neutral-800 hover:bg-blue-800 hover:text-white transition duration-300"> <span className="pr-6">Register</span> <ArrowRight className="w-4 h-4" /> </Button>
+      <div className="relative flex flex-col items-center justify-center">
+        <div className="flex justify-start mt-5 play-font">
+            <Button className="bg-[#284AA3] text-white font-medium ml-0 py-3  hover:bg-blue-300 transition duration-300"><span className="pr-18">Login</span> <ArrowRight className="w-4 h-4" /></Button>
+            <Button className="bg-white text-neutral-800 font-medium py-2 ml-4  border border-neutral-800 hover:bg-blue-800 hover:text-white transition duration-300"> <span className="pr-6">Register</span> <ArrowRight className="w-4 h-4" /> </Button>
+        </div>
+        <div className="flex justify-start mt-7 barlow-font text-xs">
+          <div className="relative">
+            <img src={ArrowM.src} alt="Magenta Arrow" className="absolute " />
+            <img src={ArrowB.src} alt="Blue Arrow" className="absolute bottom-[0px] right-[0px]" />
+            <img src={star1.src} alt="Star 1" className="absolute bottom-[0px] right-[0px]" />
+            <img src={star2.src} alt="Star 2" className="absolute bottom-[0px] right-[0px]" />
+          </div>
+          <h4 className="text-pink-600 ml-9">Stop wasting time with outdated processes and use Labchat today!</h4>
+          <h4 className="text-blue-800 mr-8">Want to start saving time? Signup today!</h4>
+        </div>
       </div>
-      <div className="flex justify-start mt-7 barlow-font text-xs">
-        <h4 className="text-pink-600 ml-9">Stop wasting time with outdated processes and use Labchat today!</h4>
-        <h4 className="text-blue-800 mr-8">Want to start saving time? Signup today!</h4>
-      </div>
-
-      <div className="flex flex-col items-center justify-center mt-20">
+    
+      <div className="flex flex-col items-center justify-center mt-7">
+        <img src={aboutStar.src} alt="Star" />
         <h2 className="text-black font-bold text-4xl play-font">About Labchat</h2>
         <h3 className="text-neutral-500 font-bold text-l mb-5 play-font">Reclaiming time, empowering science.</h3>
         <p className="text-neutral-800 font-barlow text-xs text-left ml-5 mr-5">At LabChat, we believe labs should be hubs of innovation—not bogged down by inefficiencies. Our platform centralises lab flows into one seamless hub, eliminating the chaos of scattered communications, inventory mishaps, and scheduling conflicts. With LabChat, researchers reclaim valuable hours lost to outdated processes, enabling them to focus on groundbreaking discoveries.
@@ -49,7 +60,8 @@ export default function Home() {
           <h2>Universities </h2>
         </div>
       </div>
-      <h2 className="mt-20 play-font font-bold text-4xl">Why choose us?</h2>
+      <img  className="mt-10" src={chooseStar.src} alt="Star" />
+      <h2 className="mt-3 play-font font-bold text-4xl">Why choose us?</h2>
       <div className="bg-white border-gray-400 rounded-lg w-[250px] h-[250px] shadow-neutral-600 shadow-2xs mt-5 flex flex-col items-center justify-center">
         <img src={car.src} alt="car" />
         <h3 className="text-blue-800 font-play text-xl font-bold">Efficient</h3>
@@ -66,13 +78,5 @@ export default function Home() {
   </div>
   );
 }
-/*
-<div className="relative w-full h-[500px]"> {}
-        <img src={ArrowM.src} alt="Magenta Arrow" className="absolute bottom-[35px] right-[65px]" />
-        <img src={ArrowB.src} alt="Blue Arrow" className="absolute bottom-[33px] right-[11px]" />
-        <img src={star1.src} alt="Star 1" className="absolute bottom-[31px] right-[89px]" />
-        <img src={star2.src} alt="Star 2" className="absolute bottom-[33px] right-[39px]" />
-      </div>
 
-*/
 
