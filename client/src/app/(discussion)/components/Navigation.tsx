b1c912582ae2475086947a29e2689968f254a770
cole <osrunescape.colehoward@gmail.com>
2025-04-09 13:31:45 +1200

    added mock up for discussion/thread

--- client/src/app/(discussion)/components/Navigation.tsx
+++ client/src/app/(discussion)/components/Navigation.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Link from 'next/link'
 
-const Navigation = () => {
+type Breadcrumb = {
+  name: string;
+  href: string;
+};
+
+const Navigation = ({ breadcrumbs }: { breadcrumbs: Breadcrumb[] }) => {
   return (
-    <div>Navigation</div>
+    <div className="bg-blue-800 text-white px-4 py-2 rounded-xl inline-block">
+    	{ breadcrumbs.map( (crumb, idx) => (
+        	<span key={ idx }>
+				<Link href={ crumb.href }>{ crumb.name }</Link>
+				{ idx < breadcrumbs.length - 1 && <span className="mx-2">{ '>' }</span> }
+			</span>
+		)) }
+    </div>
   )
 }
 
-export default Navigation
+export default Navigation;
--- client/src/app/(discussion)/components/Title.tsx
+++ client/src/app/(discussion)/components/Title.tsx
@@ -1,9 +1,11 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import React from 'react'
-import { TopicType } from '../types/TestTypes'
 
 type TitlePropTypes = {
-    // Topic object
-    topic: TopicType,
+    // Topic name
+    topic: string,
 
     // Permission required to view the add to topic button. Can ignore for now
     permToAdd: string,
@@ -16,10 +18,20 @@ type TitlePropTypes = {
 } 
 
 const Title = ({ topic, permToAdd, bViewAll, bCategories } : TitlePropTypes) => {
+    const router = useRouter()
+
+    const handleClick = () => { 
+        const topicData = {
+            name: topic,
+        }
+        const query = new URLSearchParams(topicData).toString()
+        router.push(`/discussion/thread?${query}`)
+    }
+
     return (
         <div className="mt-12 barlow-font">
             <div className="flex justify-between items-center mb-1">
-                <h1 className="heading text-3xl font-bold ">{ topic.name }</h1>
+                <h1 onClick={ handleClick }className="heading text-3xl font-bold ">{ topic }</h1>
                 { permToAdd && <button><img src="/add_to_topic_button.svg" alt="" /></button> }
             </div>
 
@@ -31,7 +43,7 @@ const Title = ({ topic, permToAdd, bViewAll, bCategories } : TitlePropTypes) => 
                     </div>
                 }
 
-                { bViewAll && <button className="discussion-topic-filter-button">View All</button> }
+                { bViewAll && <button onClick={ handleClick } className="discussion-topic-filter-button">View All</button> }
             </div>
         </div>
     )
