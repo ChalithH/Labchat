2059fade6482d3a8ec334af5da7e32b65ca50628
cole <osrunescape.colehoward@gmail.com>
2025-04-10 13:51:51 +1200

    added user reply ui to discussion/thread page

--- client/src/app/(discussion)/discussion/thread/[id]/page.tsx
+++ client/src/app/(discussion)/discussion/thread/[id]/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useState } from 'react'
+
 import { TEST_DATA, USER_DATA } from '../../../testdata'
+
 import Navigation from '../../../components/Navigation'
 import ThreadAuthorGroup from '../../../components/ThreadAuthorGroup'
-import { match } from 'assert'
 
 
 const DiscussionThread = ({ params }:{ params: { id: string } }): React.ReactNode => {
@@ -17,7 +19,6 @@ const DiscussionThread = ({ params }:{ params: { id: string } }): React.ReactNod
     return (
         <section className="m-auto w-[90dvw] flex flex-col gap-3">
             <Navigation />
-
             <h1 className="text-3xl font-bold play-font">{ matching_thread.title }</h1>
 
             <div>
@@ -26,6 +27,20 @@ const DiscussionThread = ({ params }:{ params: { id: string } }): React.ReactNod
 
             <div className="p-4 rounded-3xl bg-blue-50">
                 <p>{ matching_thread.content }</p>
+            </div>
+
+            <div className="p-4 barlow-font rounded-3xl bg-blue-50 flex flex-col gap-4">
+                <textarea 
+                    className="bg-white p-4 w-[100%] rounded-2xl"
+                    placeholder="Type a comment" />
+                
+                <div className="w-[100%] flex justify-between items-center">
+                    <div className="">
+                        <ThreadAuthorGroup role={ USER_DATA.title } name={ USER_DATA.name } />
+                    </div>
+
+                    <button className="rounded-xl bg-sky-600 p-2 px-4 text-white">Post Reply</button>
+                </div>
             </div>
         </section>
     )
