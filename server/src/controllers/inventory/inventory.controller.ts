8171ee1040186cbf53e65993e87047dde9fcb718
Caleb Wharton <calebwharton123@gmail.com>
2025-04-15 14:09:50 +1200

    set up swagger docs

--- server/src/controllers/inventory/inventory.controller.ts
+++ server/src/controllers/inventory/inventory.controller.ts
@@ -3,6 +3,26 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+
+/**
+ * @swagger
+ * /inventory:
+ *   get:
+ *     summary: Get all lab inventory items
+ *     tags: [Inventory]
+ *     responses:
+ *       200:
+ *         description: A list of inventory items
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/LabInventoryItem'
+ *       500:
+ *         description: Failed to retrieve inventory items
+ */
+
 export const getInventory = async (req: Request, res: Response): Promise<void> => {
     try {
         const inventoryItems = await prisma.labInventoryItem.findMany();
