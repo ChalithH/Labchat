af2ae45d17a4cc357fe65b03b18a8ddf3080f775
Parin Kasabia <pkasabia0909@gmail.com>
2025-04-16 15:06:49 +1200

    added search route

--- server/src/routes/inventory.routes.ts
+++ server/src/routes/inventory.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getInventory, takeItem, getAllLowStockItems, replenishStock, getInventoryLocal } from "../controllers/inventory/inventory.controller";
+import { getInventory, takeItem, getAllLowStockItems, replenishStock, getInventoryLocal, getInventoryItemByName } from "../controllers/inventory/inventory.controller";
 
 /**
  * @swagger
@@ -10,6 +10,7 @@ import { getInventory, takeItem, getAllLowStockItems, replenishStock, getInvento
 const router = Router();
 
 router.get('/', getInventory);
+router.get('/name/:name', getInventoryItemByName);
 router.post('/take', takeItem);
 router.get('/low-stock', getAllLowStockItems);
 router.post('/replenish', replenishStock)
