b26bf730b2f53ede7013210ff28b7cab373c411d
Caleb Wharton <calebwharton123@gmail.com>
2025-04-22 12:36:54 +1200

    set up basic files for calendar endpoints

---
+++ server/src/routes/calendar.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { } from "../controllers/calendar/calendar.controller";
+
+/**
+ * @swagger
+ * tags:
+ *   name: Calendar
+ *   description: Calendar API
+ */
+const router = Router();
+
+export default router;
