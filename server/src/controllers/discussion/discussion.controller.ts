03ef57b2739d1cce4138b7061387a4a0bf2344c0
cole <osrunescape.colehoward@gmail.com>
2025-04-23 16:22:00 +1200

    stubbed out function declarations for general end points required in discussion

--- server/src/controllers/discussion/discussion.controller.ts
+++ server/src/controllers/discussion/discussion.controller.ts
@@ -2,3 +2,68 @@ import { Request, Response } from 'express';
 import { prisma } from '../..';
 
 
+/*
+ *      Get Categories
+ */ 
+
+export const getAllCategories = async (req: Request, res: Response): Promise<void> => {
+
+}
+
+
+
+/*
+ *      Create, Edit and Delete Posts
+ */ 
+
+export const createPost = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const editPost = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const deletePost = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+
+
+/*
+ *      Get Posts By Filter
+ */ 
+
+export const getPostById = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const getPostByUser = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const getPostByTitle = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const getPostByCategory = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+
+
+/*
+ *      Unique Cases
+ */ 
+
+export const getRecentPosts = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const getPopularPosts = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const getMixedPosts = async (req: Request, res: Response): Promise<void> => {
+    
+}
