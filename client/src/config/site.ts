1f2ffa172cb7a203dae3e785362fad5f48698f49
Parin Kasabia <pkasabia0909@gmail.com>
2025-04-27 22:23:10 +1200

    updated landing page for desktop

---
+++ client/src/config/site.ts
@@ -0,0 +1,46 @@
+export type SiteConfig = {
+    name: string;
+    description: string;
+    navItems: {
+        title: string;
+        href: string;
+    }[];
+};
+
+export const loggedOutsiteConfig: SiteConfig = {
+    name: "Labchat",
+    description: "Labchat is a web application that allows users to chat with each other in real-time.",
+    navItems: [
+        {
+            title: "Home",
+            href: "#home",
+        },
+        {
+            title: "About",
+            href: "#about",
+        },
+        {
+            title: "Contact",
+            href: "#contact",
+        },
+    ],
+};
+
+export const loggedInsiteConfig: SiteConfig = {
+    name: "Labchat",
+    description: "Labchat is a web application that allows users to chat with each other in real-time.",
+    navItems: [
+        {
+            title: "Home",
+            href: "/home",
+        },
+        {
+            title: "About",
+            href: "/about",
+        },
+        {
+            title: "Contact",
+            href: "/contact",
+        },
+    ],
+};
