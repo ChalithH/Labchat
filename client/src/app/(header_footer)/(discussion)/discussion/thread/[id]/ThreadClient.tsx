6ae52b11174841142daadaf523b6d3e9a3828cc7
cole <osrunescape.colehoward@gmail.com>
2025-04-30 10:21:31 +1200

    converted discussion thread and topic views into client components

---
+++ client/src/app/(header_footer)/(discussion)/discussion/thread/[id]/ThreadClient.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import { TEST_DATA, FIRST_USER_DATA } from '@/app/testdata'
+
+import Navigation from '../../../components/Navigation'
+import ThreadAuthorGroup from '@/components/discussion/ThreadAuthorGroup'
+import { Button } from '@/components/ui/button'
+
+
+const ThreadClient = async ({ params }:{ params: { id: string } }) => {
+    const { id } = await params
+    const thread_id = parseInt(id, 10)
+
+    const matching_thread = TEST_DATA.flatMap(topic => topic.threads)
+        .find(thread => thread.id === thread_id);
+        
+    if (!matching_thread)
+        return <p>Not found</p>
+    
+    return (
+        <main className="m-auto w-[90dvw] barlow-font flex flex-col gap-3">
+            <Navigation />
+            <h1 className="text-3xl font-bold play-font">{ matching_thread.title }</h1>
+
+            <div className="flex justify-between items-center">
+                <ThreadAuthorGroup role={ FIRST_USER_DATA.title } name={ FIRST_USER_DATA.name } size={ 48 } />
+                
+                <div className="text-right text-sm">
+                    <p>Posted { matching_thread.post_date }</p>
+                    <p>Last activity { matching_thread.last_activity }</p>
+                </div>
+            </div>
+
+            <div className="p-4 rounded-3xl bg-blue-50">
+                <p>{ matching_thread.content }</p>
+            </div>
+
+            <div className="p-4 barlow-font rounded-3xl bg-blue-50 flex flex-col gap-4 my-8">
+                <textarea 
+                    className="bg-white p-4 w-[100%] rounded-2xl"
+                    placeholder="Type a comment" />
+                
+                <div className="w-[100%] flex justify-between items-center">
+                    <div>
+                        <ThreadAuthorGroup role={ FIRST_USER_DATA.title } name={ FIRST_USER_DATA.name } size={ 42 } />
+                    </div>
+
+                    {/* <button className="rounded-xl bg-sky-600 p-2 px-4 text-white">Post Reply</button> */}
+                    <Button variant="outline">Post Reply</Button>
+                </div>
+            </div>
+
+            <div className='flex justify-between pb-[24px]'>
+                <h1 className="play-font w-[90dvw] m-auto text-3xl font-bold">Replies</h1>
+                <img 
+					className="rotate-270"
+					src="/play_arrow_filled_black.svg" 
+					alt="Drop down button to see replies" />
+            </div>
+
+            { matching_thread.replies.map( reply =>
+                <div key={ reply.id }
+                    className="p-4 barlow-font rounded-3xl bg-blue-50 flex flex-col gap-4">
+                    
+                    <div className="flex justify-between items-start">
+                        <ThreadAuthorGroup role={ FIRST_USER_DATA.title } name={ FIRST_USER_DATA.name } size={ 42 } />
+                        <p className="text-sm">Posted { reply.post_Date }</p>
+                    </div>
+
+                    <p>{ reply.content }</p>
+                </div>
+            ) }
+        </main>
+    )
+}
+
+export default ThreadClient
--- client/src/app/(header_footer)/(discussion)/discussion/topic/[id]/page.tsx
+++ client/src/app/(header_footer)/(discussion)/discussion/topic/[id]/page.tsx
@@ -1,38 +1,23 @@
 import React from 'react'
 
-import Navigation from '../../../components/Navigation'
-import Title from '../../../components/Title'
-import Thread from '../../../../../../components/discussion/Thread'
-import { TEST_DATA } from '@/app/testdata';
+import TopicClient from './TopicClient'
+import setUsersLastViewed from '@/lib/set_last_viewed'
+import getUserFromSessionServer from '@/lib/get_user_server'
+import { redirect } from 'next/navigation'
 
 
-const DiscussionTopic = async ({ params }:{ params: { id: string } }) => {
+const DiscussionTopic = async ({ params }:{ params: { id: number }}) => {
     const { id } = await params
-    const topic_id = parseInt(id, 10)
-
-    const matching_topic = TEST_DATA.find(topic => topic.id === topic_id)
-
-    if (!matching_topic){
-        return 'No topic found'
+    setUsersLastViewed(`/discussion/topic/${ id }`)
+    
+    const user = await getUserFromSessionServer()
+    if (!user) {
+        redirect('/home')
     }
 
     return (
-        <main className="m-auto w-[90dvw]">
-            <div className="mb-2">
-                <Navigation />
-            </div>
-
-            <div className="mb-8">
-                <Title b_categories={ true } b_view_all={ false } perm_to_add='*' topic={ matching_topic }/>
-            </div>
-
-            { matching_topic?.threads.map( (thread, idx, arr) => (
-                <div key={ idx } className={ idx !== arr.length - 1 ? "mb-6" : "" }>
-                    <Thread key={ idx } thread={ thread } b_show_blurb={ true }/>
-                </div>
-            ))}
-        </main>
+      <TopicClient params={ {id: `${ id }`} } />
     )
-}
+  }
 
 export default DiscussionTopic
