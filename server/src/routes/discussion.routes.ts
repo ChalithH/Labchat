6ed5da97f953a4b77463ae4ffcd050848b532e33
Mark-McNaught <156684209+Mark-McNaught@users.noreply.github.com>
2025-05-02 00:00:43 +1200

    All discussion reply endpoints apart from getRepliesByPost working successfully

--- server/src/routes/discussion.routes.ts
+++ server/src/routes/discussion.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { createPost, deletePost, editPost, getPostById, getPostsByCategory, getPostsByMember, getPostsByTitle } from '../controllers/discussion/post.controller'
-
+import { getReplyById, getRepliesByPost, createReply, editReply, deleteReply,} from '../controllers/discussion/reply.controller'
 /**
  * @swagger
  * tags:
@@ -19,6 +19,13 @@ router.put('/post/:id', editPost)
 router.get('/category-posts/:id', getPostsByCategory)
 router.get('/member-posts/:id', getPostsByMember)
 router.post('/title-posts', getPostsByTitle)
+
+// /api/discussion/reply
+router.get('/reply/:id', getReplyById)
+//getRepliesByPost route needed to add
+router.post('/reply', createReply)
+router.put('/reply/:id', editReply)
+router.delete('/reply/:id', deleteReply)
 
 
 export default router
