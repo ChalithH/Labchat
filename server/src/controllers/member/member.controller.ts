bca35bac6f95681586531ea37fc1defb362609e3
Mark-McNaught <156684209+Mark-McNaught@users.noreply.github.com>
2025-05-20 19:40:40 +1200

    Implemented a general search component for both members and inventory which dynamically fetches filter criteria from db. Now working on implementing search and filter for discussion pages

--- server/src/controllers/member/member.controller.ts
+++ server/src/controllers/member/member.controller.ts
@@ -37,3 +37,44 @@ export const getMemberByUserId = async (req: Request, res: Response): Promise<vo
     res.status(500).json({ error: 'Failed to retrieve lab member' });
   }
 };
+
+/**
+ * @swagger
+ * /member/statuses:
+ *   get:
+ *     summary: Get all member statuses
+ *     tags: [Members]
+ *     responses:
+ *       200:
+ *         description: A list of member statuses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   statusName:
+ *                     type: string
+ *       500:
+ *         description: Failed to retrieve member statuses
+ */
+export const getStatuses = async (req: Request, res: Response): Promise<void> => {
+  try {
+    console.log("Fetching member statuses...");
+
+    const statuses = await prisma.status.findMany({
+      select: {
+        id: false,
+        statusName: true,
+        statusWeight: false
+      }
+    });
+
+    res.status(200).json(statuses);
+  } catch (error) {
+    console.error("Error retrieving statuses:", error);
+    res.status(500).json({ error: 'Failed to retrieve member statuses' });
+  }
+};
+
