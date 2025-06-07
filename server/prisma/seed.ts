d96780fcec06a60767d4918d96c46aaf22bd625e
Parin Kasabia <pkasabia0909@gmail.com>
2025-06-07 14:19:01 +1200

    addded event status seed data tp prod

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -38,11 +38,12 @@ async function main() {
     "labRole.json",       // independent table
     "contact.json",       // Depends on user
     "itemTag.json",       // Independent table
-    "item.json",          // Independent table 
     "postReaction.json",  // Independent table
     "replyReaction.json",  // Independent table
     "postTag.json",       // Independent table
     "eventType.json",       // Independent table
+    "eventStatus.json",    // Independent table
+
     ]
   } else {
     dataDirectory = path.join(__dirname, "devSeedData");
